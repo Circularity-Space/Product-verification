c4aa69f07542990f2cb03aa5d1cdedba66dc4bc0
PaymasterMax <duncansantiago18@gmail.com>
2021-09-16 03:17:39 -0700

    patch auto-gen by /usr/local/sbin/gm under account cipher

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,21 @@
 let submit = document.querySelector(".submit")
-let mail_checker = ()=>{
-		let uname = document.querySelector(".username").value
-		let password = document.querySelector(".password").value
-		if (uname === '' || password === '') return
-		
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
+
+let check_input = ()=>{
+		let email = document.querySelector(".username").value
+		let password = document.querySelector(".password").value
+		if (email === '' || password === '') return
+		return validateEmail(email)
+}
+
 submit.addEventListener('click',(e)=>{
 	e.preventDefault();
-	mail_checker();
+	if (check_input()) {
+				console.log("success");
+	}else{
+		console.log('invalid email');
+	}
 })
