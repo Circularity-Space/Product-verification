8fd09fb0bbb971f28d8652a382ea44bf4815f469
PaymasterMax <duncansantiago18@gmail.com>
2021-09-23 11:11:42 -0700

    patch auto-gen by /usr/local/sbin/gm under account cipher

---
+++ assets/js/detail.js
@@ -0,0 +1,32 @@
+let close_map = document.querySelector(".close_map")
+let map_container = document.querySelector(".product_map")
+let _open_map = document.querySelector("._open_map")
+let back_icon = document.querySelector(".back_icon")
+let like = document.querySelector(".like")
+
+close_map.addEventListener('click',(e)=>{
+		map_container.classList.remove('slide_in_class')
+		map_container.classList.add('slide_out_class')
+})
+_open_map.addEventListener('click',(e)=>{
+	map_container.classList.add('slide_in_class')
+	map_container.classList.remove('slide_out_class')
+})
+
+back_icon.addEventListener('click',(e)=>{window.history.back()})
+like.addEventListener('click',(e)=>{alert('liked');})
+// Initialize and add the map
+function initMap() {
+		// The location of Uluru
+		const uluru = { lat: -25.344, lng: 131.036 };
+		// The map, centered at Uluru
+		const map = new google.maps.Map(document.querySelector("._real_map"), {
+				zoom: 4,
+				center: uluru,
+		});
+		// The marker, positioned at Uluru
+		const marker = new google.maps.Marker({
+				position: uluru,
+				map: map,
+		});
+}
