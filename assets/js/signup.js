ec8f99704255d1a41ce37842648828740184ede1
PaymasterMax <duncansantiago18@gmail.com>
2021-09-23 08:26:00 -0700

    patch auto-gen by /usr/local/sbin/gm under account cipher

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,13 +1,3 @@
-let storeHub = localStorage
-storeHub.setItem("_user_info",JSON.stringify({
-		"_g":null,
-		"_age":null,
-		"_pref":null
-}))
-
-let _curren_view = 1
-let _target = ""
-
 let _collector_more_details = document.querySelector("._collector_more_details")
 let mobi = document.querySelector(".mobi")
 let login = document.querySelector(".login")
@@ -17,21 +7,51 @@ let _all_next = document.querySelectorAll("._next")
 let _boolean = document.querySelectorAll("._boolean p")
 let _images_wrapper = document.querySelectorAll("._images_wrapper img")
 let _age = document.querySelector("._age input")
+let fetch_btn = document.querySelector(".fetch_btn")
 
 let _all_children = _collector_more_details.children
+let _curren_view = 1
+let _target = ""
+
+let userProfile = ( function() {
+		let storeHub = localStorage
+		let _user_info = (storeHub.getItem("userProfile")) ? JSON.parse(storeHub.getItem("userProfile")) : { "pref":[]}
+		storeHub.setItem("userProfile",JSON.stringify({"pref":[]}))
+
+		let saveData = ()=>{
+			storeHub.setItem(
+						"userProfile",JSON.stringify(_user_info)
+			 )
+		}
+
+		let obj = {}
+		obj.update_userinfo = (info)=>{
+			for( var item in _user_info) {
+						if (String(item) == String(Object.keys(info)[0])) {
+							if (item == "pref") {
+								console.log("here we come.",_user_info[item]);
+								_user_info[item].push(info[item])
+								_user_info[item] = [...new Set(_user_info[item])]
+								saveData()
+								return
+							}
+								_user_info[item] = info[item]
+								saveData()
+								return
+						}
+			};
+			let key = Object.keys(info)[0]
+			_user_info[key] = info[key]
+			saveData()
+		}
+		obj.get_userinfo = ()=>{return _user_info;}
+		obj.get_field = (field) =>{return _user_info[field]}
+		obj.clearData = ()=>{_user_info={"pref":[]};saveData();}
+		return obj
+})()
 
 _age.addEventListener("change",(e)=>{
-					let _updated = JSON.parse(storeHub.getItem("_user_info"))
-					_updated['_age'] = e.target.value
-					storeHub.setItem("_user_info",JSON.stringify(_updated))
-})
-
-_boolean[0].addEventListener("click",(e)=>{
-				e.target.parentNode.setAttribute("gender","male")
-})
-
-_boolean[1].addEventListener("click",(e)=>{
-				e.target.parentNode.setAttribute("gender","female")
+	userProfile.update_userinfo({"age":e.target.value});
 })
 
 mobi.addEventListener("click",(e)=>{
@@ -39,14 +59,44 @@ mobi.addEventListener("click",(e)=>{
 	_all_children[0].classList.add('_show')
 	})
 
-_all_next[0].addEventListener("click",(e)=>{
-					let gender = e.target.parentNode.querySelector("._boolean").getAttribute("gender")
-					if (gender) {
-						let _updated = JSON.parse(storeHub.getItem("_user_info"))
-						console.log(_updated);
-						_updated['_g'] = gender
+_images_wrapper.forEach((item, i) => {
+			item.addEventListener("click",(e)=>{
+				let _tick = document.createElement("div")
+				_tick.classList.add("_tick")
 
-						storeHub.setItem("_user_info",JSON.stringify(_updated))
+				let first = document.createElement("div")
+				first.classList.add("first")
+				_tick.appendChild(first)
+
+				let second = document.createElement("div")
+				second.classList.add("second")
+				_tick.appendChild(second)
+				// e.target.parentNode.appendChild(_tick)
+
+				console.log(	e.target.parentNode.parentNode);
+					userProfile.update_userinfo({"pref":e.target.getAttribute("diet")})
+				e.target.parentNode.parentNode.setAttribute("diet",e.target.getAttribute("diet"))
+			})
+});
+
+_google_auth.addEventListener('click',(e)=>{
+		console.log("authenticating via google");
+})
+_facebook_auth.addEventListener('click',(e)=>{
+		console.log("authenticating via facebook");
+})
+login.addEventListener('click',(e)=>{window.location = 'login.html'})
+
+let checkDataValid = (info)=>{return (info !== "")? true:false}
+
+_all_next[0].addEventListener("click",(e)=>{
+			let uname = e.target.parentNode.querySelectorAll("input")
+
+					if (checkDataValid(uname[0].value) && checkDataValid(uname[1].value)) {
+						console.log("valid");
+						let _updated = {"fname":uname[0].value}
+						userProfile.update_userinfo({"fname":uname[0].value})
+						userProfile.update_userinfo({'lname':uname[1].value})
 									if (_curren_view==0){
 										_all_children[_curren_view].setAttribute("style","display:none")
 											_curren_view+=1
@@ -60,13 +110,17 @@ _all_next[0].addEventListener("click",(e)=>{
 					}
 					return
 })
+_boolean[0].addEventListener("click",(e)=>{userProfile.update_userinfo({"gender":"m"})})
+_boolean[1].addEventListener("click",(e)=>{userProfile.update_userinfo({"gender":"f"})})
+let checkIfFilled = (info)=>{
+	if (userProfile.get_field(info) !== "" && userProfile.get_field(info) != undefined) {
+			return true
+	}
+	return false;
+}
 _all_next[1].addEventListener("click",(e)=>{
-			let diet = e.target.parentNode.querySelector("._images_wrapper").getAttribute("diet")
-			if (diet) {
-			let _updated = JSON.parse(storeHub.getItem("_user_info"))
-			_updated['_pref'] = diet
-			storeHub.setItem("_user_info",JSON.stringify(_updated))
-
+			let bool = checkIfFilled("gender")
+			if (bool) {
 				if (_curren_view==0){
 					_all_children[_curren_view].setAttribute("style","display:none")
 					_all_children[_curren_view].setAttribute("style","display:block")
@@ -83,46 +137,46 @@ _all_next[1].addEventListener("click",(e)=>{
 })
 
 _all_next[2].addEventListener("click",(e)=>{
-	let parsed = JSON.parse(localStorage.getItem("_user_info"))
-
-	let req = fetch(_target,{
-		"method":"POST",
-		headers:{
-				"Content-Type":"application/json",
-		},
-		body:{
-								"_g":(parsed['_g'])?parsed['_g']:"male",
-								"age":(parsed['_age'])?parsed['_age']:10,
-								"_pref":(parsed['_pref'])?parsed['_pref']:10,
-						}
-	})
-	console.log("sending backend request");
+	let age = e.target.parentNode.querySelector("input").value
+	if (age !== undefined && age !=="" && checkIfFilled("age") !=="" && checkIfFilled("age") !==undefined) {
+		if (_curren_view==0){
+			_all_children[_curren_view].setAttribute("style","display:none")
+			_all_children[_curren_view].setAttribute("style","display:block")
+			_curren_view+=1
+			return
+		}
+		_all_children[_curren_view-1].setAttribute("style","display:block")
+		_all_children[_curren_view-1].setAttribute("style","display:none")
+		_all_children[_curren_view].setAttribute("style","display:none")
+		_all_children[_curren_view].setAttribute("style","display:block")
+		_curren_view+=1
+	}
+	return
 })
+_all_next[3].addEventListener("click",(e)=>{
+	let pass = e.target.parentNode.querySelectorAll("input")
+		if (pass[0].value === pass[1].value&&pass[0].value !== "") {
+			userProfile.update_userinfo({"password":pass[0].value})
+			if (_curren_view==0){
+				_all_children[_curren_view].setAttribute("style","display:none")
+				_all_children[_curren_view].setAttribute("style","display:block")
+				_curren_view+=1
+				return
+			}
+			_all_children[_curren_view-1].setAttribute("style","display:block")
+			_all_children[_curren_view-1].setAttribute("style","display:none")
+			_all_children[_curren_view].setAttribute("style","display:none")
+			_all_children[_curren_view].setAttribute("style","display:block")
+			_curren_view+=1
+		}
+		return
+ })
 
-_images_wrapper.forEach((item, i) => {
-			_images_wrapper[i].addEventListener("click",(e)=>{
-				let _tick = document.createElement("div")
-				_tick.classList.add("_tick")
+	_all_next[4].addEventListener("click",(e)=>{
+		userProfile.clearData()
+		console.log("sending data");
+			return
+	 })
 
-				let first = document.createElement("div")
-				first.classList.add("first")
-				_tick.appendChild(first)
-
-				let second = document.createElement("div")
-				second.classList.add("second")
-				_tick.appendChild(second)
-				// e.target.parentNode.appendChild(_tick)
-
-				console.log(	e.target.parentNode.parentNode);
-
-				e.target.parentNode.parentNode.setAttribute("diet",e.target.getAttribute("diet"))
-			})
-});
-
-_google_auth.addEventListener('click',(e)=>{
-		console.log("authenticating via google");
-})
-_facebook_auth.addEventListener('click',(e)=>{
-		console.log("authenticating via facebook");
-})
-login.addEventListener('click',(e)=>{window.location = 'login.html'})
+fetch_btn.addEventListener("click",(e)=>{console.log("Hello floaters");})
+let fetch_more = ()=>{}
