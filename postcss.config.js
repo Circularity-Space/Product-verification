219652f27ace68540e8b93cd88cba44c928dd85d
kelvin ngure <kkarisjuiced@gmail.com>
2021-10-01 16:30:22 +0300

    new phase of register html

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,10 @@
 const tailwindcss = require('tailwindcss')
 module.exports = {
   plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+  theme: {
+    colors: {
+      primary: '#5c6ac4',
+      secondary: '#ecc94b',
+    },
+  },
 }
